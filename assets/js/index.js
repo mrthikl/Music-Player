3b6100c65e84dcc1fdb740a4a848c405976d05f1
ThiNguyen <dragon_thi46@yahoo.com>
2022-04-28 21:13:17 +0700

    build project

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const songs = [{
+        name: 'song1',
+        singer: 'Phuong Trang',
+        path: './assets/music/song1.mp3',
+        image: './assets/image/song1.webp'
+    },
+    {
+        name: 'song2',
+        singer: 'Hoang Ton',
+        path: './assets/music/song2.mp3',
+        image: './assets/image/song2.webp'
+    },
+];
